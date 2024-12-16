381e938da0a38163ef1763880075a7b7e6b358f5
Daniel Yacobi <danieljoshua.yacobi@post.runi.ac.il>
2024-12-16 13:14:03 +0200

    Initial commit

--- ColorDemo.java
+++ ColorDemo.java
@@ -47,12 +47,6 @@ public class ColorDemo {
 			                         { black, white, black, white } ,
 			                         { white, black, white, black } ,
 		                           };
-		for (int i = 0; i < miniChessBoard.length; i++) {
-			for (int j = 0; j < miniChessBoard[i].length; j++) {
-				print(miniChessBoard[i][j]);
-			}
-			System.out.println();
-		}
 	}
 
     /**
